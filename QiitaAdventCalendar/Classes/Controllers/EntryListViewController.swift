7dcf64d29baaeb3025bfc7e42127af782525a116
Koichiro Oishi <bs@v7.com>
2017-07-01 22:17:42 +0900

    change tab space 4

--- QiitaAdventCalendar/Classes/Controllers/EntryListViewController.swift
+++ QiitaAdventCalendar/Classes/Controllers/EntryListViewController.swift
@@ -10,83 +10,83 @@ import UIKit
 import SafariServices
 
 class EntryListViewController: UIViewController {
-  
-  @IBOutlet weak var tableView: UITableView!
-
-  var calendar: CalendarEntity!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.delegate = self
-    tableView.dataSource = self
-    tableView.rowHeight = UITableViewAutomaticDimension
     
-    let nib = UINib(nibName: EntryListTableViewCell.cellIdentifier, bundle: nil)
-    tableView.register(nib, forCellReuseIdentifier: EntryListTableViewCell.cellIdentifier)
-  
-    navigationItem.title = calendar.title
+    @IBOutlet weak var tableView: UITableView!
     
-    EntryManager.scrapingCalendarItems(calendar, completion: {
-      self.tableView.reloadData()
-    })
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
-
+    var calendar: CalendarEntity!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        let nib = UINib(nibName: EntryListTableViewCell.cellIdentifier, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: EntryListTableViewCell.cellIdentifier)
+        
+        navigationItem.title = calendar.title
+        
+        EntryManager.scrapingCalendarItems(calendar, completion: {
+            self.tableView.reloadData()
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 }
 
 extension EntryListViewController: UITableViewDataSource {
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: EntryListTableViewCell.cellIdentifier) as! EntryListTableViewCell
-    let item = calendar.items[indexPath.row]
-    cell.date.text = item.date.replacingOccurrences(of: "12 / ", with: "")
-
-    if item.entryTitle.isEmpty {
-      cell.title.text = item.comment
-    } else {
-      cell.title.text = item.entryTitle
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: EntryListTableViewCell.cellIdentifier) as! EntryListTableViewCell
+        let item = calendar.items[indexPath.row]
+        cell.date.text = item.date.replacingOccurrences(of: "12 / ", with: "")
+        
+        if item.entryTitle.isEmpty {
+            cell.title.text = item.comment
+        } else {
+            cell.title.text = item.entryTitle
+        }
+        
+        cell.name.text = item.authorName
+        cell.icon.sd_setImage(with: URL(string: item.authorIconURL))
+        return cell
     }
-
-    cell.name.text = item.authorName
-    cell.icon.sd_setImage(with: URL(string: item.authorIconURL))
-    return cell
-  }
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return calendar.items.count
-  }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return calendar.items.count
+    }
+    
 }
 
 extension EntryListViewController: UITableViewDelegate {
-
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
-    if calendar.items[indexPath.row].entryURL.isEmpty {
-      tableView.deselectRow(at: indexPath, animated: true)
-      return
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if calendar.items[indexPath.row].entryURL.isEmpty {
+            tableView.deselectRow(at: indexPath, animated: true)
+            return
+        }
+        
+        if #available(iOS 9.0, *) {
+            if let url = URL.init(string: calendar.items[indexPath.row].entryURL) {
+                let safariView = SFSafariViewController.init(url: url)
+                self.present(safariView, animated: true, completion: nil)
+            }
+        } else {
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: String(describing: EntryWebViewController.self)) as! EntryWebViewController
+            vc.item = calendar.items[indexPath.row]
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
     }
-
-    if #available(iOS 9.0, *) {
-      if let url = URL.init(string: calendar.items[indexPath.row].entryURL) {
-        let safariView = SFSafariViewController.init(url: url)
-        self.present(safariView, animated: true, completion: nil)
-      }
-    } else {
-      let vc = self.storyboard?.instantiateViewController(withIdentifier: String(describing: EntryWebViewController.self)) as! EntryWebViewController
-      vc.item = calendar.items[indexPath.row]
-      self.navigationController?.pushViewController(vc, animated: true)
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
     }
-  }
-  
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return UITableViewAutomaticDimension
-  }
-
-  func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-    return UITableViewAutomaticDimension
-  }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
 }
--- QiitaAdventCalendar/Classes/Controllers/EntryWebViewController.swift
+++ QiitaAdventCalendar/Classes/Controllers/EntryWebViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class EntryWebViewController: UIViewController {
-  
-  @IBOutlet weak var webView: UIWebView!
-
-  var item: EntryEntity!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    navigationItem.title = item.entryTitle
     
-    let url = URL(string: item.entryURL)!
-    webView.loadRequest(URLRequest(url: url))
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
-  
+    @IBOutlet weak var webView: UIWebView!
+    
+    var item: EntryEntity!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = item.entryTitle
+        
+        let url = URL(string: item.entryURL)!
+        webView.loadRequest(URLRequest(url: url))
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 }
