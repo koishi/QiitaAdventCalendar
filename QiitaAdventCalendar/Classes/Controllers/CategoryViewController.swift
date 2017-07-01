75fe690dea2af4132c0ac56e1d471143aa226f0c
Koichiro Oishi <bs@v7.com>
2017-07-01 22:07:26 +0900

    Category asynchronous

--- QiitaAdventCalendar/Classes/Controllers/CategoryViewController.swift
+++ QiitaAdventCalendar/Classes/Controllers/CategoryViewController.swift
@@ -9,64 +9,67 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
-  
-  @IBOutlet weak var tableView: UITableView!
-
-  fileprivate let cellIdentifier = "Cell"
-
-  var year: String?
-  
-  var categories = [CategoryEntity]()
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.delegate = self
-    tableView.dataSource = self
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
-    navigationItem.title = year
-
-    guard self.categories.count == 0 else {
-      return
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    fileprivate let cellIdentifier = "Cell"
+    
+    var year: String?
+    
+    var categories = [CategoryEntity]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        navigationItem.title = year
+        
+        guard self.categories.count == 0 else {
+            return
+        }
+        
+        CategoryManager.scrapingCategories(year!) {
+            categories in
+            self.categories = categories
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
+        }
     }
-    CategoryManager.scrapingCategories(year!, completion:  {
-      categories in
-      self.categories = categories
-      self.tableView.reloadData()
-    })
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 }
 
 // MARK: - UITableViewDataSource
 
 extension CategoryViewController: UITableViewDataSource {
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)!
-
-    let category = self.categories[indexPath.row]
-    cell.textLabel?.text = category.title
-    return cell
-  }
-
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.categories.count
-  }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)!
+        
+        let category = self.categories[indexPath.row]
+        cell.textLabel?.text = category.title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.categories.count
+    }
+    
 }
 
 // MARK: - UITableDelegate
 
 extension CategoryViewController: UITableViewDelegate {
-
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    let vc = self.storyboard?.instantiateViewController(withIdentifier: String(describing: CalendarViewController.self)) as! CalendarViewController
-    vc.category = self.categories[indexPath.row]
-    self.navigationController?.pushViewController(vc, animated: true)
-  }
-  
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: String(describing: CalendarViewController.self)) as! CalendarViewController
+        vc.category = self.categories[indexPath.row]
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
--- QiitaAdventCalendar/Classes/Models/CategoryManager.swift
+++ QiitaAdventCalendar/Classes/Models/CategoryManager.swift
@@ -11,24 +11,27 @@ import Ji
 
 class CategoryManager: NSObject {
 
-  static func scrapingCategories(_ year:String, completion:(_ categories: [CategoryEntity]) -> Void) {
+  static func scrapingCategories(_ year:String, completion: @escaping (_ categories: [CategoryEntity]) -> Void) {
 
-    var categories = [CategoryEntity]()
-    let jiDoc = Ji(htmlURL: URL(string: "http://qiita.com/advent-calendar/\(year)/categories")!)
-    if let bodyNode = jiDoc?.xPath("//body")!.first {
-      let contentDivNode = bodyNode.xPath("div[@id='main']/div[@class='container adventCalendarBody']/div[@class='row']/div[@class='col-lg-3 col-md-4 col-sm-6']/div[@class='adventCalendarCard']")
-      for childNode in contentDivNode {
-
-        let category = CategoryEntity()
-        if let title = childNode.firstChildWithName("a")?.content {
-          category.title = title
+    DispatchQueue.global().async {
+        var categories = [CategoryEntity]()
+        let jiDoc = Ji(htmlURL: URL(string: "http://qiita.com/advent-calendar/\(year)/categories")!)
+        if let bodyNode = jiDoc?.xPath("//body")!.first {
+            let contentDivNode = bodyNode.xPath("div[@id='main']/div[@class='container adventCalendarBody']/div[@class='row']/div[@class='col-lg-3 col-md-4 col-sm-6']/div[@class='adventCalendarCard']")
+            for childNode in contentDivNode {
+                
+                let category = CategoryEntity()
+                if let title = childNode.firstChildWithName("a")?.content {
+                    category.title = title
+                }
+                if let url = childNode.firstChildWithName("a")?.attributes["href"] {
+                    category.url = url
+                }
+                categories.append(category)
+            }
         }
-        if let url = childNode.firstChildWithName("a")?.attributes["href"] {
-          category.url = url
-        }
-        categories.append(category)
-      }
+        completion(categories)
     }
-    completion(categories)
+
   }
 }
